2749798081d81aa5f05822935e798ccdae0e4f1e
Lars Kappert <lars@webpro.nl>
2022-03-18 21:54:12 -0300

    Remove unused package

--- scripts/mdast/parser.js
+++ scripts/mdast/parser.js
@@ -1,6 +1,5 @@
 import { unified } from 'unified';
 import remarkParse from 'remark-parse';
-import remarkGfm from 'remark-gfm';
 import front from 'remark-frontmatter';
 
-export default unified().use(remarkParse).use(front).use(remarkGfm);
+export default unified().use(remarkParse).use(front);
