1662b33309c44354404d5d8b73bc810b81ea9a4e
Lars Kappert <lars@webpro.nl>
2023-09-11 21:16:48 +0200

    Add content validators

---
+++ src/content/config.ts
@@ -0,0 +1,29 @@
+import { defineCollection, z } from 'astro:content';
+
+const page = defineCollection({
+  schema: z.object({
+    title: z.string().optional(),
+    description: z.string().optional(),
+    published: z.coerce.date().optional(),
+    modified: z.coerce.date().optional(),
+    // heroImage: z.string().optional()
+  }),
+});
+
+const references = page;
+
+const articles = defineCollection({
+  schema: z.object({
+    title: z.string().optional(),
+    description: z.string(),
+    published: z.coerce.date(),
+    modified: z.coerce.date().optional(),
+    draft: z.boolean().optional(),
+    image: z.string().optional(),
+    tags: z.string().optional(),
+  }),
+});
+
+const scraps = articles;
+
+export const collections = { articles, scraps, references };
