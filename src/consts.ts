70653c18b5723cff511ad3915b36b7cd9bde11da
Lars Kappert <lars@webpro.nl>
2023-10-23 14:33:14 +0200

    Fix up a few layout issues

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,3 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
 export const NAME = 'WebPro';
 export const AUTHOR = 'Lars Kappert';
 export const BLOG_NAME = 'Frontend Ramblings';
--- src/remark/heading.ts
+++ src/remark/heading.ts
@@ -12,6 +12,8 @@ export const moveTitle: RemarkPlugin = () => (tree: Root, file: VFile) => {
   const title = tree.children.find(node => node.type === 'heading' && node.depth === 1);
   if (title) {
     file.data.astro.frontmatter.title = toString(title);
-    tree.children = tree.children.filter(node => node !== title); // TODO Remove properly
+    if (file.history.some(value => /\/(articles|scraps)\//.test(value))) {
+      tree.children = tree.children.filter(node => node !== title); // TODO Remove properly
+    }
   }
 };
