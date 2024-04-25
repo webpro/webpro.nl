3c2214b533da47e1d8e78590e709b181d07bbdb3
Lars Kappert <lars@webpro.nl>
2024-04-25 11:06:01 +0200

    Update some config/lockfile

--- knip.ts
+++ knip.ts
@@ -1,8 +1,4 @@
 export default {
+  entry: ['ec.config.mjs'],
   ignoreDependencies: ['canvaskit-wasm', 'sharp'],
-  compilers: {
-    mdx: (text: string) => [...text.matchAll(/import[^;]+/g)].join('\n'),
-    astro: (text: string) => [...text.matchAll(/import[^;]+/g)].join('\n'),
-    css: (text: string) => [...text.matchAll(/(?<=@)import[^;]+/g)].join('\n'),
-  },
 };
